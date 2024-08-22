27a477b0972b74a820f9c245d819fc767a9e1c7f
Islon Scherer <i_desouzascherer@apple.com>
2024-08-22 16:02:30 +0200

    add C bindings

---
+++ bindings/c/tree-sitter-pkl.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_PKL_H
+#define TREE_SITTER_PKL_H
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_pkl();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // TREE_SITTER_PKL_H
