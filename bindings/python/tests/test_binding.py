ecc25a2b54e0e116420a3976406eac5cfe44b55f
Jen Basch <jbasch@apple.com>
2025-11-03 15:19:27 -0800

    Upgrade to Node 24

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
-import tree_sitter
+from tree_sitter import Language, Parser
 import tree_sitter_pkl
 
 
 class TestLanguage(TestCase):
     def test_can_load_grammar(self):
         try:
-            tree_sitter.Language(tree_sitter_pkl.language())
+            Parser(Language(tree_sitter_pkl.language()))
         except Exception:
             self.fail("Error loading Pkl grammar")
